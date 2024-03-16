8dcc5b5ee26439c02a8ef3d6d349632e8dadafd1
Notion Madlad <140834035+notionmadlad@users.noreply.github.com>
2024-03-16 09:41:54 +0000

    Update Vercel v1.3.8

--- src/libs/utils/convert.ts
+++ src/libs/utils/convert.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 
-export default function(htmlContent) {
+export default function(htmlContent: string) {
   const base64EncodedHtml = Buffer.from(htmlContent).toString('base64')
   const dataUrl = `data:text/html;base64,${base64EncodedHtml}`
 
