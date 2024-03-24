2ea8b8719a2fc5d5617d406d595b47b555c54ade
Notion Madlad <140834035+notionmadlad@users.noreply.github.com>
2024-03-24 08:13:10 +0000

    Update Vercel v1.8.4

--- src/libs/rss.tsx
+++ src/libs/rss.tsx
@@ -3,17 +3,21 @@ import React from "react"
 import ReactDOMServer from "react-dom/server"
 import { CONFIG } from "../../site.config"
 import NotionRenderer from "../routes/Detail/components/NotionRenderer"
+import { getPosts } from "../apis/notion-client/getPosts"
+import { getRecordMap } from "../apis/notion-client/getRecordMap"
 
 async function createFeedContent(post: any) {
+  const recordMap = await getRecordMap(post.id)
   const content = ReactDOMServer.renderToString(
-    <NotionRenderer recordMap={await post.recordMap} />
+    <NotionRenderer recordMap={await recordMap} />
   )
 
   const regexExp = /<div class="notion-collection-row"><div class="notion-collection-row-body"><div class="notion-collection-row-property"><div class="notion-collection-column-title"><svg.*?class="notion-collection-column-title-icon">.*?<\/svg><div class="notion-collection-column-title-body">.*?<\/div><\/div><div class="notion-collection-row-value">.*?<\/div><\/div><\/div><\/div>/g
   return content.replace(regexExp, '')
 }
 
-export async function generateRSS(posts: any) {
+export async function generateRSS() {
+  const posts = await getPosts()
   const year = new Date().getFullYear()
   const feed = new Feed({
     title: CONFIG.blog.title,
--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,11 +1,9 @@
 import { generateRSS } from "../libs/rss"
-import usePostsQuery from "../hooks/usePostsQuery"
 import { NextApiResponse } from "next" 
 
 export async function getServerSideProps ({ res }: { res: NextApiResponse }) {
   res.setHeader('Content-Type', 'text/xml')
-  const posts = await usePostsQuery()
-  const xmlFeed = await generateRSS(posts)
+  const xmlFeed = await generateRSS()
   res.write(xmlFeed)
   res.end()
   return {
